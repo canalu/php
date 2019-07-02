03120b3ae43a324b489fc7af1eacab407a6256b3
Carlos Nepomuceno <carlos.nep07@gmail.com>
2019-07-02 19:34:45 -0300

    Traduzi outro texto

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+	echo "Hello World";
+
+	// git commit -m "o que vc quiser commitar" - realiza um comentário no seu código.
+	// git add --all - Adiciona todos os arquivos presentes na sua pasta.
+	// git add exemplo.php - adiciona uma página específica na sua pasta no github
+
+	/* projetos no Github  */
+
+	// git remote add origin https://github.com/canalu/php.git - exemplo de endereço gerado no github. 
+	// git config --global user.name "Carlos Nepomuceno" - Exemplo de nome a ser exibido no Github.
+	// git config --global user.email "carlos.nep07@gmail.com" - Exemplo de Email para acessar a conta.
+	// git push origin master - literalmente empurrar todos os arquivos para o Servidor.
+
+
+?>
